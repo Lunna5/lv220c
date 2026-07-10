06bd07cfe2a0cae525a1492809fd36fe0304ed51
Lunna5 <git@lunna.dev>
2026-07-10 20:34:15 +0200

    feat: add lun_file header for .lun file format support

---
+++ include/lun_file.h
@@ -0,0 +1,58 @@
+//
+// Created by lunna on 10/7/26.
+//
+
+#ifndef LV220C_LUN_FILE_H
+#define LV220C_LUN_FILE_H
+
+#include <stdint.h>
+
+typedef int32_t fixed32;
+typedef int16_t fixed16;
+
+#define LUNNA_MAGIC_BYTES "LUNN"
+#define VERSION 1
+
+typedef struct {
+    char magic[4];
+    uint32_t version;
+
+    uint32_t num_textures;
+    uint32_t num_vertices;
+    uint32_t num_faces;
+    uint32_t num_entities;
+
+    uint32_t offset_textures;
+    uint32_t offset_vertices;
+    uint32_t offset_faces;
+    uint32_t offset_entities;
+} LunHeader;
+
+typedef struct {
+    char name[64];
+} LunTexture;
+
+typedef struct {
+    fixed32 x, y, z;
+} LunVertex;
+
+typedef struct {
+    uint16_t vertex_indices[3];
+
+    fixed16 u[3];
+    fixed16 v[3];
+
+    uint16_t texture_id;
+};
+
+typedef struct {
+    uint8_t type;
+    fixed32 x, y, z;
+
+    uint16_t param1;
+    uint16_t param2;
+} LunEntity;
+
+
+
+#endif //LV220C_LUN_FILE_H
